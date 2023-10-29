0ffc048e8b6bcaa08a756f51db4c62b4c0ac768b
elluop <148719155+elluop@users.noreply.github.com>
2023-10-29 22:42:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,22 +10,22 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 # VALUES REQUIRED FOR XBOTS
-API_ID = 18136872
-API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
-CMD_HNDLR = getenv("CMD_HNDLR", default=".")
+API_ID = 29499310
+API_HASH = "d49ecebafbaa772a60404ec555b9cbb9"
+CMD_HNDLR = getenv(".", default=".")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 
-BOT_TOKEN = getenv("BOT_TOKEN", default=None)
-BOT_TOKEN2 = getenv("BOT_TOKEN2", default=None)
-BOT_TOKEN3 = getenv("BOT_TOKEN3", default=None)
-BOT_TOKEN4 = getenv("BOT_TOKEN4", default=None)
-BOT_TOKEN5 = getenv("BOT_TOKEN5", default=None)
-BOT_TOKEN6 = getenv("BOT_TOKEN6", default=None)
-BOT_TOKEN7 = getenv("BOT_TOKEN7", default=None)
-BOT_TOKEN8 = getenv("BOT_TOKEN8", default=None)
-BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
-BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
+BOT_TOKEN = getenv("6460262962:AAF2_EKwujowJYIc6IOocKmcIQfjaBcvjx4", default=None)
+BOT_TOKEN2 = getenv("6739346442:AAHrv7JvJH3JkuyjVRK9l_8eHVNvA1wcRRU", default=None)
+BOT_TOKEN3 = getenv("6795643222:AAEjx5lFtIUIU1TFilv1hhF-c9xcki1oDPk", default=None)
+BOT_TOKEN4 = getenv("6816427422:AAF6ULTSLGRjXQ3ipuBnwRPQNHbYb6Jyth0", default=None)
+BOT_TOKEN5 = getenv("6767746936:AAG09wIQNldKiM35hvZotqNGrBc_l9mfO8c", default=None)
+BOT_TOKEN6 = getenv("6538530505:AAFoCGElhEPQuxojvmqU35gyv2sGHTZ6wl4", default=None)
+BOT_TOKEN7 = getenv("6915607745:AAFAPQj3nSqGtfv4IbbJ70gvuj1Z5rTvKB0", default=None)
+BOT_TOKEN8 = getenv("6746360736:AAG2tx-bVzq7ubAOHpJDVxsvUM-TIfO0VRo", default=None)
+BOT_TOKEN9 = getenv("6559355457:AAEMQB26EGU0GMf0xtIrzmKGY1Mp4EODSeg", default=None)
+BOT_TOKEN10 = getenv("6357647861:AAGbOmKJfn0zsKCnKN4bDCoRFyWZRFE4G08", default=None)
 
 SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="5518687442").split()))
 for x in ALTRON:
